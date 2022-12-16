dbfda994a941ee4d24fcbe61000662d04eda9b1d
lunarwar2 <101807623+lunarwar2@users.noreply.github.com>
2022-12-16 20:55:26 +0700

    Create empatnamlapan.js

---
+++ empatnamlapan.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '8cbc39143f76fb16dd232de7640f6c31',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://hailofficemeasure.com/8cbc39143f76fb16dd232de7640f6c31/invoke.js"></scr' + 'ipt>');
